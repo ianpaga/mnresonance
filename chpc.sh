15cd43332d7b617771f0759fd27803fd410e5f58
Ian Padilla-Gay <57350668+ianpaga@users.noreply.github.com>
2024-05-05 22:21:30 -0700

    Update chpc.sh

--- chpc.sh
+++ chpc.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# compile mu100-400by400by100_E***.cpp in the chosen start_$2 directory
-
-cd start_$2/
-c++ -O4 -I/groups/astro/ianpaga/boost_1_71_0 -fopenmp $1.cpp -o $1.out
-##c++ -O4 -Wall -I/groups/astro/ianpaga/boost_1_71_0 -fopenmp $1.cpp -o $1.out
+c++ -O4 -I/groups/astro/ianpaga/boost_1_71_0 -fopenmp mu100-400by400by100_Ev0_azym_grid.cpp -o mu100-400by400by100_Ev0_azym_grid.out
